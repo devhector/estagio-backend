9e2e921e8c79e2756c5caa60edc80b565438c34b
devhector <hectorhu17@gmail.com>
2021-03-04 18:01:25 -0300

    organized

--- server/server.js
+++ server/server.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const app = express();
 const router = require("./routes");
+
+const app = express();
 
 app.use(express.json());
 app.use(router);
-
-app.get('/', (request, response) => {
-    response.send("<h1>O pai tá on</h1>");
-});
 
 app.listen(3131, () => {
     console.log("server on!");
