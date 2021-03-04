675bd006e0274023316b8fc2c08dc20616012055
devhector <hectorhu17@gmail.com>
2021-03-04 17:59:16 -0300

    routes for navers created

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const NaversController = require("../controllers/NaversController");
+
+router.get('/api/navers', NaversController.index);
+router.post('/api/navers', NaversController.store);
+
 module.exports = router;
