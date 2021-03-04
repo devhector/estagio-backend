c76b3427c124c915dc16b0ab641100de42826b94
devhector <hectorhu17@gmail.com>
2021-03-04 11:31:31 -0300

    add migrations path

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,11 @@ module.exports = {
       database: process.env.DB_NAME,
       user:     process.env.DB_USER,
       password: process.env.DB_PASSWORD
+    },
+    migrations: {
+      tableName: "nave_migrations",
+      directory: `${__dirname}/server/database/migrations`
     }
-  },
+  }
 
 };
