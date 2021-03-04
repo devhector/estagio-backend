ffa79df918f82400d76ae30139b399c32c9ed082
devhector <hectorhu17@gmail.com>
2021-03-04 11:41:20 -0300

    create schema db

---
+++ server/database/migrations/20210304105712_create_table_navers.js
@@ -0,0 +1,12 @@
+
+exports.up = knex => knex.schema.createTable('navers', table => {
+
+    table.increment('id');
+    table.text('name').notNullable();
+    table.date('birthdate').notNullable();
+    table.date('admission_date').notNullable();
+    table.text('job_role').notNullable();
+
+});
+
+exports.down = knex => knex.schema.dropTable('navers');
