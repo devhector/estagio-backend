8cb4f0fe3363f5eba9e15d0a98666fe1f7c68d6e
devhector <hectorhu17@gmail.com>
2021-03-06 10:26:58 -0300

    add timestamp and organize

--- server/database/migrations/20210304105712_create_table_navers.js
+++ server/database/migrations/20210304105712_create_table_navers.js
@@ -2,10 +2,13 @@
 exports.up = knex => knex.schema.createTable('navers', table => {
 
     table.increments('id');
-    table.text('name').notNullable();
+    table.string('name').notNullable();
+    table.string('job_role').notNullable();
     table.date('birthdate').notNullable();
     table.date('admission_date').notNullable();
-    table.text('job_role').notNullable();
+
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
 
 });
 
