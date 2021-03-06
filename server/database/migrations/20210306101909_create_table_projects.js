d3d2dad2bd113e6ed6d93fc0fc8a90764014687c
devhector <hectorhu17@gmail.com>
2021-03-06 10:26:03 -0300

    create table projects

---
+++ server/database/migrations/20210306101909_create_table_projects.js
@@ -0,0 +1,12 @@
+
+exports.up = knex => knex.schema.createTable('projects', table => {
+
+    table.increments('id');
+    table.string('name').notNullable();
+
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+
+});
+
+exports.down = knex => knex.schema.dropTable('projects');
