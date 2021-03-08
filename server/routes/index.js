fbeae44eb3a97856fa92c7f16c5c945e08d9d95d
devhector <hectorhu17@gmail.com>
2021-03-08 15:40:19 -0300

    all routes created

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const NaversController = require("../controllers/NaversController");
+const ProjectsController = require("../controllers/ProjectsController");
+
 
 router.get('/api/navers', NaversController.index);
+router.get('/api/navers/:id', NaversController.show);
 router.post('/api/navers', NaversController.store);
+
+
+router.get('/api/projects', ProjectsController.index);
+router.get('/api/projects/:id', ProjectsController.show);
+router.post('/api/projects', ProjectsController.store);
+
 
 module.exports = router;
