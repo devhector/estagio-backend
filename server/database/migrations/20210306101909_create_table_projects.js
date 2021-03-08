9d7051afd085411679962584303593855bd8b5f0
devhector <hectorhu17@gmail.com>
2021-03-08 15:38:49 -0300

    add primary key id

--- server/database/migrations/20210306101909_create_table_projects.js
+++ server/database/migrations/20210306101909_create_table_projects.js
@@ -1,7 +1,7 @@
 
 exports.up = knex => knex.schema.createTable('projects', table => {
 
-    table.increments('id');
+    table.increments('id').primary();
     table.string('name').notNullable();
 
     table.timestamp('created_at').defaultTo(knex.fn.now());
